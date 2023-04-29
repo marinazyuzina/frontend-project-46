929f3e87a9b3a2c1814ecbeee248c5f3f78ed03b
Marina Zyuzina <zmarina526@gmail.com>
2023-04-29 19:22:50 +0300

    update  src/index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
 import { readFileSync } from 'fs';
 import _ from 'lodash';
+import path from 'path';
+import { cwd } from 'node:process';
+
 
 const getDataParsed = (path) => {
     const data = readFileSync(path, {encoding:'utf8', flag:'r'});
     return JSON.parse(data);
 };
 
-const compareValues = (key, file1, file2) => {
-    if (Object.hasOwn(file1, key) === Object.hasOwn(file2, key)) {
-      if (file1.key === file2.key) {
-        return file1[key];
-      }
-     // как вернуть оба значения, если ключ есть в обоих объектах, но с разными значениями?
-    }
-  if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-    return file1[key];
-  }
-return file2[key];
-};
 
 const genDiff = (filepath1, filepath2) => {
 
-const data1 = getDataParsed(filepath1);
-const data2 = getDataParsed(filepath2);
+const resolvedFilepath1 = path.resolve(cwd(), filepath1);
+const resolvedFilepath2 = path.resolve(cwd(), filepath2);
+
+const data1 = getDataParsed(resolvedFilepath1);
+const data2 = getDataParsed(resolvedFilepath2);
 
 const keys1 = Object.keys(data1);
 const keys2 = Object.keys(data2);
 const keys =_.sortBy(_.union(keys1, keys2));
 
-const result = keys.reduce((acc, key) => {
-acc[key] = compareValues(key, data1, data2);
-return acc;
-}, {});
-return result;
+const result = keys.map((key) => {
+if (!_.has(data1, key)) {
+  return `+ ${key}: ${data2[key]}`;
+}
+if (!_.has(data2, key)) {
+  return `- ${key}: ${data1[key]}`;
+}
+if (_.isEqual(data1[key], data2[key])) {
+  return `  ${key}: ${data1[key]}`;
+}
+return [[`- ${key}: ${data1[key]}`], [`+ ${key}: ${data2[key]}`]];
+});
+return _.flatten(['{', ...result, '}']).join('\n');
 };
 
 export default genDiff;
